4f07852e3d22ddfe3a8f38a0be602716ba685f7c
willanysilva <willany_lp@hotmail.com>
2019-11-26 20:11:00 -0300

    Add menu items

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/servicos">Serviços</Link>
+      <Link to="/criacoes">Criações</Link>
+      <Link to="/eventos">Eventos</Link>
+    </div>
+  );
 }
