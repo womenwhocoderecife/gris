7dbfdda5c04ef3a7a24fae310bd2943fd5ed0e3b
Nelkisa Matias <nelkisa.matias@mv.com.br>
2020-03-09 14:37:02 -0300

    correção no layout do menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { ul } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../assets/logo-horizontal.png";
 
 export default function Navbar() {
   return (
-    <div className="menu">
+    <div className="header">
       <div className="logo">
         <img src={logo} alt="Logotipo da ONG Gris" />
       </div>
-      <div>
-        <Link to="/" className="menu-itens">
-          Início
-        </Link>
-        <Link to="/quemsomos" className="menu-itens">
-          Quem somos
-        </Link>
-        <Link to="/acoes" className="menu-itens">
-          Ações
-        </Link>
-        <Link to="/calendario" className="menu-itens">
-          Calendário
-        </Link>
-        <Link to="/quero-contribuir" className="menu-itens">
-          Quero Contribuir
-        </Link>
+      <div className="menu">
+        <ul className="menu-itens">
+          <li to="/">
+            Início
+          </li>
+          <li to="/">
+            Quem somos
+          </li>
+          <li to="/">
+            Ações
+          </li>
+          <li to="/">
+            Calendário
+          </li>
+          <li to="/">
+            Quero Contribuir
+            <ul className="sub-menu-itens">
+              <li>
+                Quero Doar
+              </li>
+              <li>
+                Quero ser Voluntário
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </div>
   );
