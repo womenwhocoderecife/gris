4b547c1f3cd9223e854c5043d04b647d0fcbacea
Dari Diniz <darianydiniz@gmail.com>
2020-06-18 17:51:12 -0300

    Add responsivity to 'Quem somos' and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,10 @@ import './Footer.css'
 export default function Footer() {
     return (
         <section id="footer">
-            © Gris - Espaço Solidário Casa Maria de Lourdes. 2020 |
-          Desenvolvido por <a href="http://womendevprojects.com.br/">Women Dev Projects</a>.
+            <div className="text-footer">
+                <div className="gris-name"><b>© Gris - Espaço Solidário Casa Maria de Lourdes</b>. 2020</div>
+                <div className="wdp-name">Desenvolvido por <b><a href="http://womendevprojects.com.br/" className="link"> Women Dev Projects</a></b>.</div>
+           </div>
         </section>
     );
 }
--- src/pages/Home/Components/WhereWeAre/index.js
+++ src/pages/Home/Components/WhereWeAre/index.js
@@ -15,12 +15,6 @@ export default function Services() {
                 <iframe
                     title="mapa"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.573998710754!2d-34.96229708583211!3d-8.04277849420867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1bdb19c9a137%3A0x96c0a7ea168c3fa8!2sR.%20Diogo%20Barbosa%20Machado%2C%2015%20-%20V%C3%A1rzea%2C%20Recife%20-%20PE%2C%2050810-290!5e0!3m2!1spt-BR!2sbr!4v1581179143138!5m2!1spt-BR!2sbr"
-                    style={{
-                        width: "45%",
-                        height: "300px",
-                        frameborder: "0px",
-                        border: "none"
-                    }}
                 />
                 <div className="map-description">
                     <div className="map-description-itens">
