a869b778f30bca00ff15bf0425b153447cc4a217
willanysilva <willany_lp@hotmail.com>
2020-06-19 18:12:54 -0300

    Change css class

--- src/pages/Home/Components/Banner/index.js
+++ src/pages/Home/Components/Banner/index.js
@@ -3,7 +3,7 @@ import "./Banner.css";
 
 export default function Banner() {
   return (
-    <section className="banner">
+    <section className="home-banner">
       <div className="banner-video">
         <iframe
           src="https://www.youtube.com/embed/4euEVkkLVYY"
