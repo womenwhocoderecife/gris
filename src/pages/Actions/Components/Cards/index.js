d4c36bf94c78dd6302a537a75c989efc52759599
Thaysa <thaysacgms@gmail.com>
2020-06-18 22:56:27 -0300

    Adjusting image size

--- src/pages/Actions/Components/Cards/index.js
+++ src/pages/Actions/Components/Cards/index.js
@@ -1,37 +1,15 @@
 import React from "react";
 import './cards.css';
-import actions3 from '../../../../assets/images/actions-3.jpg';
-import actions2 from '../../../../assets/images/actions-2.jpg';
-import actions1 from '../../../../assets/images/actions-1.jpg';
 
-export default function Card() {
+
+export default function Card({ image, title, paragraph, width}) {
     return (
-        <div className='cards'>
-
-            <div className='card'>
-                <div className='card-img fix'><img className='image' src={actions1} /></div>
+        <div className='action-card'>
+                <div className='card-img'><img className='image' src={image} style={{ width: width }}/></div>
                 <div className='card-info'>
-                    <h1 className='card-title'>Acompanhamento pedagógico</h1>
-                    <p className='card-paragraph'>Atendimento de crianças e adolescentes com dificuldades e problemas de aprendizagem.</p>
+                    <h1 className='card-title'>{title}</h1>
+                    <p className='card-paragraph'>{paragraph}</p>
                 </div>
-            </div>
-
-            <div className='card-two'>
-                <div className='card-img'><img className='image' src={actions2} /></div>
-                <div className='card-info'>
-                    <h1 className='card-title'>Reforço escolar</h1>
-                    <p className='card-paragraph'>Trabalhando as pontencialidades de cada criança de acordo com suas áreas de habilidade/interesse.</p>
-                </div>
-            </div>
-
-            <div className='card-three'>
-                <div className='card-img'><img className='image' src={actions3} /></div>
-                <div className='card-info'>
-                    <h1 className='card-title'>Acompanhamento social</h1>
-                    <p className='card-paragraph'>Cada família é acompanhada em sua totalidade; oficinas de produção criativa para crianças e mães.</p>
-                </div>
-            </div>
-
         </div>
     );
 }
--- src/pages/Actions/index.js
+++ src/pages/Actions/index.js
@@ -1,13 +1,34 @@
 import React from "react";
-import Banner from "./Components/Banner";
 import Card from "./Components/Cards"
 import './Actions.css'
+import actions1 from '../../assets/images/card1.png';
+import actions2 from '../../assets/images/card2.png';
+import actions3 from '../../assets/images/card3.png';
 
 export default function Creations() {
     return (
         <section>
-            <div className="title">AÇÕES</div>
-            <Card/>
+            <div className="title">Ações</div>
+            <div className='cards-actions'>
+            <Card
+             image={actions1}
+             title="Acompanhamento pedagógico"
+             paragraph="Atendimento de crianças e adolescentes com dificuldades e problemas de aprendizagem."
+             width="100%"
+            />
+            <Card
+             image={actions2}
+             title="Reforço escolar"
+             paragraph="Trabalhando as pontencialidades de cada criança de acordo com suas áreas de habilidade/interesse."
+             width="90%"
+            />
+            <Card
+             image={actions3}
+             title="Acompanhamento social"
+             paragraph="Cada família é acompanhada em sua totalidade; oficinas de produção criativa para crianças e mães."
+             width="90%"
+            />
+            </div>
         </section>
     );
 }
